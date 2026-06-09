4ffe75bd48e1a320fa3529372c93c251c081e66e
t4k020 <t4k020@gmail.com>
2026-06-09 13:30:40 -0300

    routing

--- Libreria Online/front/src/app/app.routes.ts
+++ Libreria Online/front/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeUser } from './layouts/home-user/home-user';
 import { ProveedorHome } from './modules/proveedor/pages/proveedor-home/proveedor-home';
+import { Register } from './layouts/register/register';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: 'proveedor',
     component: ProveedorHome
+  },
+  { 
+    path: 'register', 
+    component: Register
   }
 
 ];
