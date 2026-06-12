c99784c4d184afa00ce440bc94ec68a730914644
Sebastian <Sebastian@DESKTOP-CDQ7UH7>
2026-06-12 16:23:15 -0300

    agregada de logica en el back de crear oferta

---
+++ Libreria Online/back/src/routes/ofertaLibro-router/ofertaLibro.router.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { OfertaLibroController } from '../../controller/ofertaLibro.controller.js'; // Ajustá la ruta a tu carpeta
+
+const router = Router();
+const controller = new OfertaLibroController();
+
+router.get('/', controller.getOfertas);
+
+router.get('/:id', controller.getOfertaPorId);
+
+router.post('/', controller.createOferta);
+
+router.put('/admin/contraofertar/:id', controller.contraofertaAdmin);
+
+router.put('/proveedor/contraofertar/:id', controller.contraofertaProveedor);
+
+router.put('/aceptar/:id', controller.aceptarOferta);
+
+export default router;
